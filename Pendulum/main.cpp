13595591eb423d9fe47e941489fecb045aeec94c
Matias Araoz <araozmatias2010@gmail.com>
2026-03-09 18:07:48 -0300

    Updated pendulum logic

--- Pendulum/main.cpp
+++ Pendulum/main.cpp
@@ -1,10 +1,11 @@
 #include "raylib.h"
 #include "raymath.h"
+#include <cstddef>
 
 
 #define WIDTH 800
 #define HEIGHT 600
-#define GRAVITY 9800
+#define GRAVITY 1500
 
 float sind(float degrees){
 	return sin(degrees * PI/180 );
@@ -20,18 +21,24 @@ class Pendulum{
 	float angularSpeed;
 	float angularAcceleration;
 	Vector2 origin;
+	bool isRoot;
 
-	Pendulum(float length, Vector2 origin, float angle ){
+	Pendulum(float length, Vector2 origin, float angle, bool isRoot = true ){
 		this->origin = origin;
 		this->length = length;
 		this->angle = angle;
+		this->isRoot = isRoot;
 	}
 
-	void Update(float dt){
-		
-		angularAcceleration = -(GRAVITY/length) * sind(angle);
+	void Update(float dt, Pendulum* father = nullptr ){
+		angularAcceleration = -1 * (GRAVITY/length) * sind(angle);
 		angularSpeed += angularAcceleration * dt;
 		angle += angularSpeed * dt;
+		if (isRoot){return;}	
+		if (father != nullptr){
+			origin.x = father->origin.x + father->length * sind(father->angle);
+			origin.y = father->origin.y + father->length * cosd(father->angle);
+		}
 	}
 
 	void Draw(){
@@ -39,6 +46,7 @@ class Pendulum{
 		float y = origin.y + length * cosd(angle);
 
 		DrawLineV(origin, (Vector2){x,y}, RED);
+		DrawCircleV((Vector2){x,y}, 10.0f, RED);
 	}
 };
 
@@ -47,7 +55,8 @@ int main(){
 
 	InitWindow(WIDTH, HEIGHT, "Pendulum");
 
-	Pendulum pendulum1 = Pendulum(100, {500,500}, 90);
+	Pendulum pendulum1 = Pendulum(100, {WIDTH/2,HEIGHT/2}, 170, true);
+	Pendulum pendulum2 = Pendulum(100, {WIDTH/3,HEIGHT/3}, 90, false);
 
 
 	SetTargetFPS(60);
@@ -55,11 +64,14 @@ int main(){
 		float dt = GetFrameTime();
 
 		pendulum1.Update(dt);
+		pendulum2.Update(dt, &pendulum1);
+
 
 		BeginDrawing();
 		ClearBackground(WHITE);
 
 		pendulum1.Draw();
+		pendulum2.Draw();
 
 		EndDrawing();
 

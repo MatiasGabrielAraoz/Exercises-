6afdf9cc07d1299ea391081eb91a0a55ddd30fdb
Matias Araoz <araozmatias2010@gmail.com>
2026-05-07 22:11:11 -0300

    a

--- GeoMati/main.c
+++ GeoMati/main.c
@@ -1,5 +1,7 @@
 #define SDL_MAIN_USE_CALLBACKS 1
 
+#include "libs/tinyexpr.h"
+
 #include <stdio.h>
 #include <printf.h>
 #include <math.h>
@@ -23,12 +25,23 @@
 
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
+
+typedef enum {
+	SIN,
+	COS,
+	TAN
+}MathType;
+
 typedef struct{
 	float amplitude;
 	float frequency;
 	SDL_FPoint* points;
+	MathType type;
+	int colorR;
+	int colorG;
+	int colorB;
 
-}Sin;
+}Function;
 
 typedef struct{
 	void* data;
@@ -36,6 +49,16 @@ typedef struct{
 	int size;
 	size_t elementSize;
 }DynamicArray;
+
+typedef struct{
+	int windowWidth;
+	int windowHeight;
+	int lastWindowWidth;
+	int lastWindowHeight;
+	char* formula;
+	DynamicArray waves;
+
+}AppData;
 
 void initArray(DynamicArray *arr, size_t elementSize, int initialCapacity){
 	arr->elementSize = elementSize;
@@ -49,7 +72,6 @@ void appendArray(DynamicArray *arr, void* element){
 		arr->capacity *= 2;
 		arr->data = SDL_realloc(arr->data, arr->elementSize * arr->capacity);
 	}
-	Sin* element2 = (Sin*)element;
 
 	void* target = (char*)arr->data + (arr->size * arr->elementSize);
 	memcpy(target, element, arr->elementSize);
@@ -60,24 +82,15 @@ void* getArray(DynamicArray *arr, int index){
 	return (char*)arr->data + (index * arr->elementSize);
 }
 
-typedef struct{
-	int windowWidth;
-	int windowHeight;
-	int lastWindowWidth;
-	int lastWindowHeight;
-	int sinsAmount;
-	DynamicArray sins;
 
-}AppData;
-
-SDL_FPoint* GetSinPoints(int windowWidth, int windowHeight, float frequency, float amplitude){
+SDL_FPoint* GetPoints(MathType type, int windowWidth, int windowHeight, float frequency, float amplitude){
 	SDL_FPoint* points = (SDL_FPoint*)SDL_malloc(sizeof(SDL_FPoint) * (windowWidth * 2));
 
 	for (int i = 0; i < windowWidth * 2; i++){
 		SDL_FPoint point;
 		float mathX = (float)i / 2.0f;
-		point.x = (float)i;
-		point.y = ((float)windowHeight / 2) - (sinf(mathX * frequency) * amplitude);
+		point.x = (float)i / 2.0f;
+		// point.y = ((float)windowHeight / 2) - (mathFunction(mathX * frequency) * amplitude);
 		
 		points[i] = point;
 	}
@@ -85,34 +98,32 @@ SDL_FPoint* GetSinPoints(int windowWidth, int windowHeight, float frequency, flo
 	if (points == NULL) return NULL;
 	return points;
 }
-SDL_FPoint* GetCosPoints(int windowWidth, int windowHeight, float frequency, float amplitude){
-	SDL_FPoint* points = (SDL_FPoint*)SDL_malloc(sizeof(SDL_FPoint) * (windowWidth * 2));
-	
-	for (int i = 0; i < windowWidth * 2; i++){
-		SDL_FPoint point;
-		float mathX = (float)i / 2.0f;
-		point.x = (float)i;
-		point.y = ((float)windowHeight /2) - (cosf(mathX * frequency) * amplitude);
-		points[i] = point;
-	}
-	if (points == NULL) return NULL;
-	return points;
 
-}
-
-Sin CreateSin(void *appstate, float amplitude, float frequency){
+Function CreateWave(void *appstate,MathType type, float amplitude, float frequency){
 	AppData *data = appstate;
-	Sin sin;
+	Function wave;
 
-	sin.frequency = frequency;
-	sin.amplitude = amplitude;
 	int windowWidth, windowHeight;
 	SDL_GetWindowSize(window, &windowWidth, &windowHeight);
-	sin.points = GetSinPoints(windowWidth, windowHeight, sin.frequency, sin.amplitude);
 
-	appendArray(&data->sins, &sin);
+	wave.frequency = frequency;
+	wave.amplitude = amplitude;
+	wave.type = type;
+	wave.points = GetPoints(type, windowWidth, windowHeight, wave.frequency, wave.amplitude);
+	if (type == SIN){
+		wave.colorR = 255;
+		wave.colorG = 0;
+		wave.colorB = 0;
+	}
+	if (type == COS){
+		wave.colorR = 0;
+		wave.colorG = 0;
+		wave.colorB = 255;
+	}
 
-	return sin;
+	appendArray(&data->waves, &wave);
+
+	return wave;
 }
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char* argv[]){ 
@@ -124,15 +135,41 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char* argv[]){
 		return SDL_APP_FAILURE;
 	}
 	AppData *data = (AppData*)SDL_calloc(1, sizeof(AppData)); SDL_GetWindowSize(window, &data->windowWidth, &data->windowHeight);
+
+	if (argc > 1){
+		data->formula = argv[1];
+
+	}
+	else{
+		SDL_Log("No se especificó ninguna fórmula");
+		return SDL_APP_FAILURE;
+	}
 	
-	initArray(&data->sins, sizeof(Sin), 2);
-	CreateSin(data, 5, 0.02f);
+	initArray(&data->waves, sizeof(Wave), 2);
+	// CreateWave(data, SIN, 50.0f, 0.02f);
+	// CreateWave(data, COS, 50.0f, 0.02f);
+
 
 	data->lastWindowWidth = 0;
 	data->lastWindowHeight = 0;
-	
 
+	int error;
+	double x_val = 1;
+	int x = 0;
+
+	te_variable vars[] = {{"x", &x_val}};
+	te_expr *expr = te_compile(data->formula, vars, 1, &error);
+
+	if (expr){
+		x_val = x;
+		float y = te_eval(expr);
+		printf("x = %d \n y = %.2f", x, y);
+		te_free(expr);
+
+	}
 	*appstate = data;
+	return SDL_APP_SUCCESS;
+
 	return SDL_APP_CONTINUE;
 }
 
@@ -145,26 +182,26 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event){
 		SDL_GetWindowSize(window, &data->windowWidth, &data->windowHeight);
 		appstate = data;
 	}
-	if (event->type == SDL_EVENT_MOUSE_WHEEL){
-		for (int i = 0; i < data->sins.size; i++){
-
-			Sin* sin = (Sin*)getArray(&data->sins, i);
-			if (sin == NULL) continue;
-
-			if (event->wheel.y > 0){
-				sin->frequency *= 1.1f;
-				sin->amplitude *= 1.1f;
-			}
-			if (event->wheel.y < 0){
-				if (sin->amplitude > 1.0f && sin->frequency > 0.001f) {
-					sin->frequency /= 1.1f;
-					sin->amplitude /= 1.1f;
-				}
-			}
-			SDL_free(sin->points);
-			sin->points = GetSinPoints(data->windowWidth, data->windowHeight, sin->frequency, sin->amplitude);
-		}
-	}
+	// if (event->type == SDL_EVENT_MOUSE_WHEEL){
+	// 	for (int i = 0; i < data->waves.size; i++){
+	//
+	// 		Wave* wave = (Wave*)getArray(&data->waves, i);
+	// 		if (wave == NULL) continue;
+	//
+	// 		if (event->wheel.y > 0){
+	// 			wave->frequency *= 1.1f;
+	// 			wave->amplitude *= 1.1f;
+	// 		}
+	// 		if (event->wheel.y < 0){
+	// 			if (wave->amplitude > 1.0f && wave->frequency > 0.001f) {
+	// 				wave->frequency /= 1.1f;
+	// 				wave->amplitude /= 1.1f;
+	// 			}
+	// 		}
+	// 		SDL_free(wave->points);
+	// 		wave->points = GetPoints(wave->type,data->windowWidth, data->windowHeight, wave->frequency, wave->amplitude);
+	// 	}
+	// }
 
 	return SDL_APP_CONTINUE;
 }
@@ -177,18 +214,18 @@ SDL_AppResult SDL_AppIterate(void *appstate){
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE_FLOAT);
 	SDL_RenderClear(renderer);
 
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE_FLOAT);
 	
-	for (int i = 0; i < data->sins.size; i++){
-		Sin* seno = (Sin*)getArray(&data->sins, i);
-		if (seno == NULL) continue;
+	for (int i = 0; i < data->waves.size; i++){
+		Function* wave = (Wave*)getArray(&data->waves, i);
+		if (wave == NULL) continue;
 		
 		if (data->lastWindowWidth != data->windowWidth || data->lastWindowHeight != data->windowHeight){
-			SDL_free(seno->points);
-			seno->points = GetSinPoints(data->windowWidth, data->windowHeight, seno->frequency, seno->amplitude);
+			SDL_free(wave->points);
+			wave->points = GetPoints(wave->type, data->windowWidth, data->windowHeight, wave->frequency, wave->amplitude);
 		}
 
-		SDL_RenderLines(renderer, seno->points, data->windowWidth*2);
+		SDL_SetRenderDrawColor(renderer, wave->colorR, wave->colorG, wave->colorB, SDL_ALPHA_OPAQUE_FLOAT);
+		SDL_RenderLines(renderer, wave->points, data->windowWidth*2);
 	}
 
 
@@ -203,14 +240,15 @@ SDL_AppResult SDL_AppIterate(void *appstate){
 void SDL_AppQuit(void* appstate, SDL_AppResult result){
 	AppData* data = appstate;
 
-	for (int i = 0; i < data->sins.size; i++){
-		Sin* sin = (Sin*)getArray(&data->sins, i);
+	for (int i = 0; i < data->waves.size; i++){
+		Function* sin = (Wave*)getArray(&data->waves, i);
 
 		if (sin->points != NULL){
 			SDL_free(sin->points);
 		}
 	}
-	SDL_free(data->sins.data);
+	SDL_free(data->waves.data);
 	SDL_free(data);
+
 
 }
